90d43df804b4409ff6aefc69ed94babcbca620bb
giova <giovanni.angeli1@stud.unifi.it>
2021-03-07 18:47:49 +0100

    Add constructor/destructor, getter/setter

--- User.h
+++ User.h
@@ -4,5 +4,22 @@
 
 #ifndef CHAT_PROJECT_USER_H
 #define CHAT_PROJECT_USER_H
+#include <iostream>
+class User {
+public:
+    explicit User(std::string &name) : name(name) {}
+    ~User();
 
+    const std::string &getName() const {
+        return name;
+    }
+
+    void setName(const std::string &name) {
+        User::name = name;
+    }
+
+private:
+    std::string name;
+
+};
 #endif //CHAT_PROJECT_USER_H
