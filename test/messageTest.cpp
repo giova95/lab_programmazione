f6676807b7ad76a0d08538fbbbe8c68cefd4628e
giova95 <giovanni.angeli1@stud.unifi.it>
2022-10-26 10:21:33 +0200

    fix all tests

--- test/messageTest.cpp
+++ test/messageTest.cpp
@@ -9,7 +9,7 @@ TEST(Message, GetterSetter){
     Message text("giovanni", "alberto", "Hi, what's up?", false );
     ASSERT_EQ(text.getSender(), "giovanni");
     ASSERT_EQ(text.getReceiver(), "alberto");
-    ASSERT_EQ(text.getText(), "Hi, what's up");
+    ASSERT_EQ(text.getText(), "Hi, what's up?");
     ASSERT_EQ(text.isRead(), false);
     text.setRead(true);
     ASSERT_EQ(text.isRead(), true);
